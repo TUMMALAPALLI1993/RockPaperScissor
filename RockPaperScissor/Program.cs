7aab4ee19f87c92381a09d531e56f60dfd800d1f
Kiran93 <tummalapalli.kirankumar@gmail.com>
2019-08-12 10:52:48 +0200

    Added comments to the program

--- RockPaperScissor/Program.cs
+++ RockPaperScissor/Program.cs
@@ -10,36 +10,56 @@ namespace RockPaperScissor
     {
         static void Main(string[] args)
         {
-            String inputPlayer, inputComputer;
+            //playerName-gets the player name as input, inputPlayer-gets the  option R/P/S from user, inputComputer- CPU choise
+            String playerName, inputPlayer, inputComputer;
             int randInput;
 
-            bool playAgain = true;            
+            bool playAgain = true;
 
+            Console.Write("Enter your name:- ");
+            playerName = Console.ReadLine();
+
+            //To get the player name
+            if (playerName.Trim()==null || playerName.Trim()=="")
+            {
+                playerName = "Player";
+            }
+
+            //To loop the game after a certain score is achieved
             while (playAgain)
             {
                 int scorePlayer = 0;
                 int scoreComputer = 0;
 
+                //Loop the game untill a particular score (3) achieved
                 while (scoreComputer < 3 && scorePlayer < 3)
                 {
                     Console.Write("Choose option Rock, Paper, Scissor.... ");
                     inputPlayer = Console.ReadLine();
 
+                    //Get a random input by CPU
                     Random random = new Random();
                     randInput = random.Next(1, 4);
 
+                    //Based on the random number generator (switch the Rock,Paper,Scissor options)
                     switch (randInput)
                     {
+                        //Implementaion if computer chooses Rock
                         case 1:
                             inputComputer = "Rock";
-                            Console.WriteLine("Computer chooses Rock");
+
+                            if (inputPlayer.ToUpper() == "Rock".ToUpper() || inputPlayer.ToUpper() == "Paper".ToUpper() || inputPlayer.ToUpper() == "Scissor".ToUpper())
+                            {
+                                Console.WriteLine("Computer chooses Rock");
+                            }
+                            
                             if (inputPlayer.ToUpper() == "Rock".ToUpper())
                             {
                                 Console.Write("Draw!!!!\n");
                             }
                             else if (inputPlayer.ToUpper() == "Paper".ToUpper())
                             {
-                                Console.Write("Player Wins!!!\n");
+                                Console.Write(playerName +" Wins!!!\n");
                                 scorePlayer++;
                             }
                             else if (inputPlayer.ToUpper() == "Scissor".ToUpper())
@@ -52,9 +72,15 @@ namespace RockPaperScissor
                                 Console.Write("Invalid Input [" + inputPlayer + "]\n");
                             }
                             break;
+
+                        //Implementaion if computer chooses Paper
                         case 2:
                             inputComputer = "Paper";
-                            Console.WriteLine("Computer chooses Paper");
+                            if (inputPlayer.ToUpper() == "Rock".ToUpper() || inputPlayer.ToUpper() == "Paper".ToUpper() || inputPlayer.ToUpper() == "Scissor".ToUpper())
+                            {
+                                Console.WriteLine("Computer chooses Paper");
+                            }
+                            
                             if (inputPlayer.ToUpper() == "Rock".ToUpper())
                             {
                                 Console.WriteLine("Computer Wins!!!!\n");
@@ -66,7 +92,7 @@ namespace RockPaperScissor
                             }
                             else if (inputPlayer.ToUpper() == "Scissor".ToUpper())
                             {
-                                Console.Write("Player Wins!!!\n");
+                                Console.Write(playerName + " Wins!!!\n");
                                 scorePlayer++;
                             }
                             else
@@ -74,12 +100,18 @@ namespace RockPaperScissor
                                 Console.Write("Invalid Input [" + inputPlayer + "]\n");
                             }
                             break;
+
+                        //Implementaion if computer chooses Scissor
                         case 3:
-                            inputPlayer = "Scissor";
-                            Console.WriteLine("Computer chooses Scissor");
+                            inputComputer = "Scissor";
+                            if (inputPlayer.ToUpper() == "Rock".ToUpper() || inputPlayer.ToUpper() == "Paper".ToUpper() || inputPlayer.ToUpper() == "Scissor".ToUpper())
+                            {
+                                Console.WriteLine("Computer chooses Scissor");
+                            }
+                            
                             if (inputPlayer.ToUpper() == "Rock".ToUpper())
                             {
-                                Console.Write("Player Wins!!!\n");
+                                Console.Write(playerName + " Wins!!!\n");
                                 scorePlayer++;
                             }
                             else if (inputPlayer.ToUpper() == "Paper".ToUpper())
@@ -96,17 +128,20 @@ namespace RockPaperScissor
                                 Console.Write("Invalid Input [" + inputPlayer + "]\n");
                             }
                             break;
+
+                        //Default switch implementation
                         default:
                             Console.Write("Invalid Entry");
                             break;
                     }
 
-                    Console.WriteLine("\n\nSCORES:\tPLAYER:\t{0}\tCPU:\t{1}\n", scorePlayer, scoreComputer);
+                    Console.WriteLine("\n\nSCORES:\t{0}:\t{1}\tCPU:\t{2}\n",playerName, scorePlayer, scoreComputer);
                 }
 
+                //Get the input from user, to continue play again the game after certain score is reahed
                 if (scorePlayer == 3)
                 {
-                    Console.WriteLine("Player Wins!!!");
+                    Console.WriteLine(playerName + " Wins!!!");
                 }
                 else if (scoreComputer == 3)
                 {
@@ -128,10 +163,7 @@ namespace RockPaperScissor
                 {
                     playAgain = false;
                 }
-
             }
-
-
         }
     }
 }
